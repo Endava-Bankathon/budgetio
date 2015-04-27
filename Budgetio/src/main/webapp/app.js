29716a369674268648e967f6000eaaff74fee3c9
Gleb Godonoga <gleb.godonoga@gmail.com>
2015-04-27 12:09:40 +0300

    Integrated gulp with maven

--- Budgetio/src/main/webapp/app.js
+++
@@ -1,3 +0,0 @@
-(function(){    
-    var app = angular.module("budgetioApp", ["ngRoute", "ui.validate", "angularFileUpload"]); 
-}());
--- Budgetio/src/main/webapp/budgetio/BudgetioController.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	var app = angular.module("budgetioApp");
-
-	var BudgetioController = function($scope, $window, figoService, giniService, FileUploader) {	
-	};
-
-	app.controller("BudgetioController", [ "$scope", "$window", "figoService", "giniService", "FileUploader", BudgetioController]);
-}());
